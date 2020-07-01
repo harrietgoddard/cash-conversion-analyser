cf28ac4f2be266c790561ef83ab2ff161f578b79
Harriet Goddard <goddardhe@gmail.com>
2020-07-01 13:17:58 +0100

    Circular progress bar installed

--- src/components/MainForm/index.js
+++ src/components/MainForm/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { Form, Col, Button } from 'react-bootstrap';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 const MainForm = () => {
+    
+    const value = 0.66;
+    
     return (
         <Form className="ml-5 mr-5">
 
@@ -65,8 +70,18 @@ const MainForm = () => {
             </Form.Row>
 
             <Button variant="primary" type="submit">
-                Submit
+                Generate report
             </Button>
+            <div 
+                style={{width: 200, height: 200}}
+                className="mt-5 mb-5"
+            >
+                <CircularProgressbar 
+                    value={value} 
+                    maxValue={1} 
+                    text={`${value * 100}%`}
+                />
+            </div>
         </Form>
     );
 };
