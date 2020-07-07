9f83e0d59eb0b6e2300490a89a984ecb8dd46fc4
Harriet Goddard <goddardhe@gmail.com>
2020-07-07 12:51:05 +0100

    reducer refactored

--- src/data/demoData.js
+++ src/data/demoData.js
@@ -1,32 +1,32 @@
 export const demoA = {
-    ebitda: 100,
+    ebitda: 5800,
     debtors: {
-        opening: 22,
-        closing: 29
+        opening: 6500,
+        closing: 8900
     },
     stock: {
-        opening: 34,
-        closing: 15
+        opening: 4300,
+        closing: 4500
     },
     creditors: {
-        opening: 65,
-        closing: 42
+        opening: 2600,
+        closing: 3100
     },
 };
 
 export const demoB = {
-    ebitda: 999,
+    ebitda: 3400,
     debtors: {
-        opening: 99,
-        closing: 88
+        opening: 2500,
+        closing: 1800
     },
     stock: {
-        opening: 77,
-        closing: 66
+        opening: 2400,
+        closing: 2600
     },
     creditors: {
-        opening: 55,
-        closing: 44
+        opening: 5600,
+        closing: 6400
     },
 };
 
