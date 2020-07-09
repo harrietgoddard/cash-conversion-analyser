958b112aa0761475d1bdb616602697e54c6dd078
Harriet Goddard <goddardhe@gmail.com>
2020-07-09 11:47:40 +0100

    Advice file added containing main commentary

--- src/components/Commentary/Commentary.js
+++ src/components/Commentary/Commentary.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import advice from '../../data/advice';
 
 const Commentary = ({ cashConversion, cashOutflows }) => {
     
@@ -6,42 +7,38 @@ const Commentary = ({ cashConversion, cashOutflows }) => {
 
     return (
         <div
-            className="d-flex flex-column align-items-center"
+            className="d-flex flex-column align-items-center m-auto"
+            style={{ maxWidth: 700 }}
         >
 
             <p 
-                style={{ maxWidth: 700 }}
-                className="ml-5 mr-5 mb-5"
+                className="ml-5 mr-5 mb-3"
             >Operating cash conversion is a measure of how well your profits have turned into cash (excluding any investing or financing cash flows). It is impacted by how you manage your working capital (collecting payments from customers, managing stock levels and paying suppliers).
             </p>
 
-            <p 
-                style={{ maxWidth: 700 }}
-                className="ml-5 mr-5 mb-5"
+            <p
+                className="ml-5 mr-5 mb-3"
             >You have an operating cash conversion of { cashConversion * 100 + "%" }. 
             { cashConversion < 0.5 || cashConversion > 1 ? null :
             " This means that the majority of your profits have turned into cash, however, since it is less than 100%, it reflects that an element of your profit is tied up in working capital." }
             </p>
 
-            <p 
-                style={{ maxWidth: 700 }}
-                className="ml-5 mr-5 mb-5"
+            <p
+                className="ml-5 mr-5 mb-3"
             >{ primaryOutflow.name !== "debtors" ? null :
-            `Your largest cash drain is your trade debtors, which increased by £${ -primaryOutflow.value }. Your customers aren't paying you fast enough. To speed up cash collections, consider asking customers to pay a deposit up front. Alternatively, tighten up your credit control procedures by hiring a credit controller, formalise the process of chasing up overdue payments, or introduce early payment discounts or late payment penalties.` }
+            `Your largest cash drain is your trade debtors, which increased by £${ -primaryOutflow.value }. ${ advice.debtors.primary } ${ advice.debtors.secondary }` }
             </p>
 
-            <p 
-                style={{ maxWidth: 700 }}
-                className="ml-5 mr-5 mb-5"
+            <p
+                className="ml-5 mr-5 mb-3"
             >{ primaryOutflow.name !== "stock" ? null :
-            `Your largest cash drain is your stock, which increased by £${ -primaryOutflow.value }. You're potentially buying too much stock. To increase stock efficiency, consider using a stock management system that reorders stock for you. Make sure to perform regular stock counts, and keep an eye on older stock to ensure it is sold before becoming obsolete.` }
+            `Your largest cash drain is your stock, which increased by £${ -primaryOutflow.value }. ${ advice.stock.primary } ${ advice.stock.secondary }` }
             </p>
 
-            <p 
-                style={{ maxWidth: 700 }}
-                className="ml-5 mr-5 mb-5"
+            <p
+                className="ml-5 mr-5 mb-3"
             >{ primaryOutflow.name !== "creditors" ? null :
-            `Your largest cash drain is your creditors, which decreased by £${ -primaryOutflow.value }. You're paying your creditors faster than you're collecting payments from customers. See if you can negotiate extended payment terms with your main suppliers. Alternatively, redress the balance by speeding up cash collections from customers. Consider asking customers to pay a deposit up front, or tighten up your credit control procedures.` }
+            `Your largest cash drain is your creditors, which decreased by £${ -primaryOutflow.value }. ${ advice.creditors.primary } ${ advice.creditors.secondary }` }
             </p>
 
         </div>
--- src/data/advice.js
+++ src/data/advice.js
@@ -1,3 +1,19 @@
-export const advice = {
-    
-}
+const advice = {
+
+    debtors: {
+        primary: "Your customers aren't paying you fast enough.",
+        secondary: "To speed up cash collections, consider asking customers to pay a deposit up front. Alternatively, tighten up your credit control procedures by hiring a credit controller, formalise the process of chasing up overdue payments, or introduce early payment discounts or late payment penalties."
+    },
+
+    stock: {
+        primary: "You're potentially buying too much stock.",
+        secondary: "To increase stock efficiency, consider using a stock management system that reorders stock for you. Make sure to perform regular stock counts, and keep an eye on older stock to ensure it is sold before becoming obsolete."
+    },
+
+    creditors: {
+        primary: "You're paying your creditors faster than you're collecting payments from customers.",
+        secondary: "See if you can negotiate extended payment terms with your main suppliers. Alternatively, redress the balance by speeding up cash collections from customers. Consider asking customers to pay a deposit up front, or tighten up your credit control procedures."
+    },
+};
+
+export default advice;
