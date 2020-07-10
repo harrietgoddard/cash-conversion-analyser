4ddba4f3039d6415344ddfae17ceb17124d5fec1
Harriet Goddard <goddardhe@gmail.com>
2020-07-10 20:00:57 +0100

    explanation of cash conversion moved to advice.js

--- src/components/Commentary/Commentary.js
+++ src/components/Commentary/Commentary.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import advice from '../../data/advice';
+import { advice, explanation } from '../../data/advice';
 
 const Commentary = ({ cashConversion, cashOutflows }) => {
     
@@ -10,35 +10,43 @@ const Commentary = ({ cashConversion, cashOutflows }) => {
             className="d-flex flex-column align-items-center m-auto"
             style={{ maxWidth: 700 }}
         >
+            <p className="ml-5 mr-5 mb-3">
 
-            <p 
-                className="ml-5 mr-5 mb-3"
-            >Operating cash conversion is a measure of how well your profits have turned into cash (excluding any investing or financing cash flows). It is impacted by how you manage your working capital (collecting payments from customers, managing stock levels and paying suppliers).
+                {/* intro */}
+                Operating cash conversion is a measure of how well your profits have turned into cash (excluding any investing or financing cash flows). It is impacted by how you manage your working capital (collecting payments from customers, managing stock levels and paying suppliers).
+
             </p>
 
-            <p
-                className="ml-5 mr-5 mb-3"
-            >You have an operating cash conversion of { cashConversion * 100 + "%" }. 
-            { cashConversion < 0.5 || cashConversion > 1 ? null :
-            " This means that the majority of your profits have turned into cash, however, since it is less than 100%, it reflects that an element of your profit is tied up in working capital." }
+            <p className="ml-5 mr-5 mb-3">
+
+                {/* operating cash conversion */}
+                You have an operating cash conversion of { cashConversion * 100 + "%" }. 
+
+                {/* explanation of operating cash conversion */}
+                { cashConversion < 0 ? explanation.negative : cashConversion < 0.5 ? explanation.lessThan50 :
+                cashConversion < 1 ? explanation.lessThan100 :
+                explanation.moreThan100 }
             </p>
 
-            <p
-                className="ml-5 mr-5 mb-3"
-            >{ primaryOutflow.name !== "debtors" ? null :
-            `Your largest cash drain is your trade debtors, which increased by £${ -primaryOutflow.value }. ${ advice.debtors.primary } ${ advice.debtors.secondary }` }
+            <p className="ml-5 mr-5 mb-3">
+
+                { primaryOutflow.name !== "debtors" ? null :
+                `Your largest cash drain is your trade debtors, which increased by £${ -primaryOutflow.value }. ${ advice.debtors.primary } ${ advice.debtors.secondary }` }
+
             </p>
 
-            <p
-                className="ml-5 mr-5 mb-3"
-            >{ primaryOutflow.name !== "stock" ? null :
-            `Your largest cash drain is your stock, which increased by £${ -primaryOutflow.value }. ${ advice.stock.primary } ${ advice.stock.secondary }` }
+            <p className="ml-5 mr-5 mb-3">
+
+                { primaryOutflow.name !== "stock" ? null :
+                `Your largest cash drain is your stock, which increased by £${ -primaryOutflow.value }. ${ advice.stock.primary } ${ advice.stock.secondary }` }
+
             </p>
 
-            <p
-                className="ml-5 mr-5 mb-3"
-            >{ primaryOutflow.name !== "creditors" ? null :
-            `Your largest cash drain is your creditors, which decreased by £${ -primaryOutflow.value }. ${ advice.creditors.primary } ${ advice.creditors.secondary }` }
+            <p className="ml-5 mr-5 mb-3">
+
+                { primaryOutflow.name !== "creditors" ? null :
+                `Your largest cash drain is your creditors, which decreased by £${ -primaryOutflow.value }. ${ advice.creditors.primary } ${ advice.creditors.secondary }` }
+                
             </p>
 
         </div>
--- src/data/advice.js
+++ src/data/advice.js
@@ -1,4 +1,16 @@
-const advice = {
+export const explanation = {
+
+    negative: " A negative cash conversion means that you made a trading loss or all your profit (and then some) is tied up in working capital",
+
+    lessThan50: " This means that the majority of your profit did not convert into cash since it is tied up in working capital.",
+
+    lessThan100: " This means that the majority of your profits have turned into cash, however, since it is less than 100%, it reflects that an element of your profit is tied up in working capital.",
+
+    moreThan100: " An operating cash conversion greater than 100% means that you generated cash from trading over and above your profit. This indicates that your customers are paying you faster than you're paying suppliers, and is a sign of a healthy business from a cash perspective - nice one!"
+
+}
+
+export const advice = {
 
     debtors: {
         primary: "Your customers aren't paying you fast enough.",
@@ -15,5 +27,3 @@ const advice = {
         secondary: "See if you can negotiate extended payment terms with your main suppliers. Alternatively, redress the balance by speeding up cash collections from customers. Consider asking customers to pay a deposit up front, or tighten up your credit control procedures."
     },
 };
-
-export default advice;
